fb7ec3f0cdff19004e62d4aa35823066ac5a6d7d
Alice <c0874203@mylambton.ca>
2023-01-18 11:14:11 -0500

    fix conflict issue.

--- Task_TeamSundry_iOS_MAD4114/Controllers/Category List/CategoryListViewController.swift
+++ Task_TeamSundry_iOS_MAD4114/Controllers/Category List/CategoryListViewController.swift
@@ -2,8 +2,6 @@
 //  CategoryListViewController.swift
 //  Task_TeamSundry_iOS_MAD4114
 //
-//  Created by Alice’z Poy on 2023-01-16.
-//
 
 import Foundation
 import UIKit
--- Task_TeamSundry_iOS_MAD4114/Controllers/Task List/TaskListViewController.swift
+++ Task_TeamSundry_iOS_MAD4114/Controllers/Task List/TaskListViewController.swift
@@ -2,8 +2,6 @@
 //  ViewController.swift
 //  Task_TeamSundry_iOS_MAD4114
 //
-//  Created by Alice’z Poy on 2023-01-12.
-//
 
 import UIKit
 
